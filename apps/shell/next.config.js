5478b74296652e06d32e65351ddaf0a8596ea29d
Kiriti <kiriti.k999@gmail.com>
2024-02-22 19:33:15 +0530

    Switch URL based on env

--- apps/shell/next.config.js
+++ apps/shell/next.config.js
@@ -1,5 +1,9 @@
-const { i18n } = require("./next-i18next.config.js");
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+
+const MFE_HOST = {
+  qa: 'https://www.google.com',
+  dev: 'http://localhost:3000'
+}
 
 module.exports = {
   output: "standalone",
@@ -11,15 +15,15 @@ module.exports = {
     localeDetection: false
   },
   webpack: (config, options) => {
+    const BASE_URL = MFE_HOST[process.env["NODE_ENV"]];
     const { isServer } = options;
     config.experiments = { topLevelAwait: true, layers: true };
     config.plugins.push(
       new NextFederationPlugin({
         name: "shell",
         remotes: {
-          home: `home@http://127.0.0.1:3001/_next/static/${
-            isServer ? "ssr" : "chunks"
-          }/remoteEntry.js`
+          // home: `home@http://127.0.0.1:3001/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`
+          home: `home@${BASE_URL}/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`
         },
         exposes: {
           "./EmptyComponent": "./src/Empty.component.tsx"
