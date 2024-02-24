28a4d753f6608753718ae206bedb2b6e627e8d0b
Kiriti Komaragiri <kiriti.k999@gmail.com>
2024-02-24 19:13:34 +0530

    up

--- shell.sh
+++ shell.sh
@@ -1,2 +1,3 @@
 #!/bin/sh
-cd apps/shell/.next && grep -rl "localhost:3001" . | xargs sed -i "s|localhost:3001|google.com|g" && node shell/server.js
+echo $1
+cd apps/shell/.next && grep -rl "localhost:3001" . | xargs sed -i "s|localhost:3001|$1|g" && node shell/server.js
