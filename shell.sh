2b1bf635ec70d3cd2b4fd49afa7a69a9f7916065
Kiriti Komaragiri <kkomaragiri@contractor.ea.com>
2024-02-26 02:46:59 +0530

    up

--- shell.sh
+++ shell.sh
@@ -3,5 +3,6 @@ echo $1
 echo $URL
 # cd apps/shell/.next && grep -rl "localhost:3001" . | xargs sed -i "s|localhost:3001|$1|g" && node shell/server.js
 find standalone/apps/shell/server.js -type f -exec sed -i "s|\${BASE_URL}|$URL|g" {} +
-find standalone/apps/shell/.next/next.config.js -type f -exec sed -i "s|\${BASE_URL}|$URL|g" {} +
+find standalone/apps/shell/.next/server/webpack-runtime.js -type f -exec sed -i "s|\${BASE_URL}|$URL|g" {} +
+find next.config.js -type f -exec sed -i "s|\${BASE_URL}|$URL|g" {} +
 node standalone/apps/shell/server.js
