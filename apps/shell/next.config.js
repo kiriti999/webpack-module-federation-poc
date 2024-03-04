bfd988130bec7f001d3228083a22a4c050436cd8
GarySmith42 <jonathanboucaud971@gmail.com>
2024-03-04 11:57:05 +0100

    setup dynamic url in environment

--- apps/shell/next.config.js
+++ apps/shell/next.config.js
@@ -1,4 +1,5 @@
 const { NextFederationPlugin } = require("@module-federation/nextjs-mf");
+const ExternalTemplateRemotesPlugin = require('external-remotes-plugin');
 
 const MFE_HOST_URL = 'http://127.0.0.1:3001'
 
@@ -15,7 +16,9 @@ module.exports = {
         name: "shell",
         remotes: {
           // home: `home@http://127.0.0.1:3001/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`
-          home: `home@${MFE_HOST_URL}/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`
+          home: `home@${MFE_HOST_URL}/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`,
+          remote1: `home@[window.app1Url]/remoteEntry.js`, //You place your variable here
+          remote2: `home@[window.app2Url]/_next/static/${isServer ? "ssr" : "chunks"}/remoteEntry.js`, /*to get back to your example*/ 
         },
         exposes: {
           "./EmptyComponent": "./src/Empty.component.tsx"
@@ -23,6 +26,9 @@ module.exports = {
         filename: "static/chunks/remoteEntry.js"
       })
     );
+
+    config.plugins.push(new ExternalTemplateRemotesPlugin());
+
     return config;
   }
 };
