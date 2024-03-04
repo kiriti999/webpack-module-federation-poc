697fa58855579166bdabb1a721337af555d23782
GarySmith42 <jonathanboucaud971@gmail.com>
2024-03-04 11:58:05 +0100

    add global variable set

--- apps/shell/src/pages/index.tsx
+++ apps/shell/src/pages/index.tsx
@@ -1,7 +1,21 @@
 import { NextPage } from "next";
+import { useEffect } from "react";
+
+declare global {
+  interface Window {
+    app1Url?: string;
+    app2Url?: string;
+  }
+}
 
 const Shell: NextPage = () => {
-  return <h1>This is shell</h1>;
+  useEffect(() => {
+      // Client-side-only code
+      window.app2Url = "http://127.0.0.1:3001" /*set the global variable for configuration*/
+    
+  }, [])
+
+  return <h1>This is shell test</h1>;
 };
 
 export default Shell;
